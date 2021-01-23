1557c39e1914bd04cb6755367eda6617a2a5f1d9
Connor Richards <c.william.richards@gmail.com>
2021-01-23 15:23:02 -0800

    ENH: add canopy metrics function

--- gedi/process_l2a.py
+++ gedi/process_l2a.py
@@ -7,7 +7,7 @@ import pandas as pd
 from typing import List
 
 
-def hf5s_to_df(
+def gedi_L2A_to_df(
     file_dir: str,
     file_paths: List[str],
     bbox: List[float],
@@ -23,7 +23,39 @@ def hf5s_to_df(
     ],
     latlayer: str = "lat_lowestmode",
     lonlayer: str = "lon_lowestmode",
-):
+) -> pd.DataFrame:
+    """
+    Given the absolute path to a directory of GEDI Level 2A h5 files, the
+    names of the h5 files, and information about the desired output, ingest
+    the h5 files and output a pd.DataFrame containing data for all the valid
+    shots within the bounding box.
+
+    The output will have an array-valued column that is designed to be handled
+    by append_canopy_metrics and then deleted from the DataFrame.
+
+    Parameters
+    ----------
+    file_dir : str
+        Path to directory containing the h5 files, including a trailing slash
+        such that {file_dir}{fname} is a valid path to a file, given fname a
+        valid file name.
+    file_paths : List[str]
+        The names of the h5 files to process.
+    bbox : List[float]
+        The coordinates of the bounding box, formatted as
+        [ul_lat, ul_lon, lr_lat, lr_lon].
+    layers : List[str]
+        The columns for the output DataFrame.
+    latlayer : str
+        The name of the latitude layer.
+    lonlayer : str
+        The name of the longitude layer.
+
+    Returns
+    -------
+    pd.DataFrame containing the user-specified contents of the h5 files for the
+    valid shots within the specified bounding box.
+    """
     df = pd.DataFrame()
     for _f_name in file_paths:
         try:
@@ -65,6 +97,106 @@ def hf5s_to_df(
     return df.reset_index(drop=True)
 
 
+def _compute_nan_percentile(a: np.ndarray, q: float) -> np.array:
+    """This code is taken directly from StackOverflow at:
+    https://stackoverflow.com/questions/60015245/numpy-nanpercentile-is-extremely-slow
+
+    For 2d masked array of roughly 10^5 x 100, this performs ~3x faster than
+    np.nanpercentile (~10s vs. 3s) and produces results which are identical.
+
+    Would be good to add two tests to verify: (1) the results of this function are
+    the same as np.nanpercentile, and (2) that this function takes less time on a
+    reasonably sized array, in case of future performance gains in nanpercentile.
+
+    Parameters
+    ----------
+    a : array_like
+        2d array for which to compute a row-wise percentile.
+    q : float
+        Percentile to compute -- allowed values are [0, 100]
+
+    Returns
+    -------
+    np.array of the q'th percentile for each row of a. Dimensions of [len(a), 1].
+    """
+    if q < 0 or q > 100:
+        raise ValueError(f"Expected a value between 0 and 100; received {q} instead.")
+    mask = (a >= np.nanmin(a)).astype(int)
+
+    count = mask.sum(axis=1)
+    groups = np.unique(count)
+    groups = groups[groups > 0]
+
+    p = np.zeros((a.shape[0]))
+    for g in range(len(groups)):
+        pos = np.where(count == groups[g])
+        values = a[pos]
+        values = np.nan_to_num(values, nan=(np.nanmin(a) - 1))
+        values = np.sort(values, axis=1)
+        values = values[:, -groups[g] :]
+        p[pos] = np.percentile(values, q, axis=1)
+    return p
+
+
+def append_canopy_metrics(df: pd.DataFrame, canopy_threshold: float) -> None:
+    """
+    The least bad option for computing these canopy metrics seems to be using
+    np.ma module to work with masked arrays. Not converting out of Pandas is
+    untenable due to computation time, and the variable dimension of rows means
+    np converts the result of applying the mask to the 2d array to a 1d array.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The DataFrame, assumed to be the output of gedi_L2A_to_df, for which
+        canopy metrics should be calculated. The provided df must contain a column
+        labeled "rh" which is array-valued.
+    canopy_threshold : float
+        The minimum value for a return to be considered a "canopy" return.
+
+    Returns
+    -------
+    None
+    """
+    rh = np.stack(df.rh.to_numpy())
+    canopy_returns = np.ma.masked_less(rh, canopy_threshold)
+
+    canopy_max = np.max(canopy_returns, axis=1)
+    canopy_min = np.min(canopy_returns, axis=1)
+    canopy_std = np.std(canopy_returns, axis=1)
+    canopy_avg = np.average(canopy_returns, axis=1)
+    dns = canopy_returns.count(axis=1)  # % all returns >= canopy threshold
+
+    canopy_returns = np.ma.filled(canopy_returns, np.nan)
+    canopy_p10 = _compute_nan_percentile(canopy_returns, 10)
+    canopy_p25 = _compute_nan_percentile(canopy_returns, 25)
+    canopy_p50 = _compute_nan_percentile(canopy_returns, 50)
+    canopy_p75 = _compute_nan_percentile(canopy_returns, 75)
+    canopy_p90 = _compute_nan_percentile(canopy_returns, 90)
+
+    d01 = np.ma.masked_outside(rh, canopy_threshold, 5).count(
+        axis=1
+    )  # % returns >= canopy, <=5m
+    d02 = np.ma.masked_outside(rh, 5, 10).count(axis=1)  # % returns >= 5m, <=10m
+    d03 = np.ma.masked_outside(rh, 10, 20).count(axis=1)  # % returns >= 10m, <=20m
+    d04 = np.ma.masked_outside(rh, 20, 30).count(axis=1)  # % returns >= 20m, <=30m
+
+    df["canopy_max"] = pd.Series(canopy_max)
+    df["canopy_min"] = pd.Series(canopy_min)
+    df["canopy_std"] = pd.Series(canopy_std)
+    df["canopy_avg"] = pd.Series(canopy_avg)
+    df["canopy_p10"] = pd.Series(canopy_p10)
+    df["canopy_p25"] = pd.Series(canopy_p25)
+    df["canopy_p50"] = pd.Series(canopy_p50)
+    df["canopy_p75"] = pd.Series(canopy_p75)
+    df["canopy_p90"] = pd.Series(canopy_p90)
+    df["dns"] = pd.Series(dns)
+    df["d01"] = pd.Series(d01)
+    df["d02"] = pd.Series(d02)
+    df["d03"] = pd.Series(d03)
+    df["d04"] = pd.Series(d04)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -98,5 +230,7 @@ if __name__ == "__main__":
         if os.path.isfile(os.path.join(args.dir, _f)) and _f.endswith(".h5")
     ]
 
-    out = hf5s_to_df(args.dir, files, bbox)
-    out.to_parquet(f"{args.dir}{args.outfile}.parquet.gzip", compression="gzip")
+    df = gedi_L2A_to_df(args.dir, files, bbox)
+    append_canopy_metrics(df, canopy_threshold=2)
+    del df["rh"]
+    df.to_parquet(f"{args.dir}{args.outfile}.parquet.gzip", compression="gzip")
